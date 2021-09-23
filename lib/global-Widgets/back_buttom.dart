0e60c9d2f65ee052a54d9d3217bb6819c7675282
Romolo Nobre <romoloferreira@icloud.com>
2021-09-23 07:45:58 +0100

    Created new Global Widget - BackButtom -

---
+++ lib/global-Widgets/back_buttom.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class BackButtom extends StatelessWidget {
+  const BackButtom({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+        color: Colors.grey.shade700,
+        shape: BoxShape.circle,
+      ),
+      child: const Icon(
+        Icons.arrow_back,
+        color: Colors.white,
+      ),
+    );
+  }
+}
