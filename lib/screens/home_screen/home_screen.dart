b67b107362ca0344c34110b6edaea34a7e6233ec
Romolo Nobre <romoloferreira@icloud.com>
2021-09-23 07:02:33 +0100

    Modified import

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-import 'body_home.dart';
+import 'components/body_home.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
