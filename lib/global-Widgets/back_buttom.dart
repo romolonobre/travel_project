de1bd69538ded20b792115385bb6ad674669d433
Romolo Nobre <romoloferreira@icloud.com>
2021-09-23 08:06:48 +0100

    Added a constracto to Backbuttom

--- lib/global-Widgets/back_buttom.dart
+++ lib/global-Widgets/back_buttom.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
 
 class BackButtom extends StatelessWidget {
-  const BackButtom({
-    Key? key,
-  }) : super(key: key);
-
+  BackButtom({required this.icon});
+  final Icon icon;
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50,
-      width: 50,
-      decoration: BoxDecoration(
-        color: Colors.grey.shade700,
-        shape: BoxShape.circle,
-      ),
-      child: const Icon(
-        Icons.arrow_back,
-        color: Colors.white,
-      ),
-    );
+        height: 50,
+        width: 50,
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.2),
+          shape: BoxShape.circle,
+        ),
+        child: IconButton(onPressed: () {}, icon: icon));
   }
 }
