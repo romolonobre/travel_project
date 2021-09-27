a2bf41e6409a25d8c682516deb7614f0c96ab409
Romolo Nobre <romoloferreira@icloud.com>
2021-09-27 10:47:32 +0100

    Modified status bar color

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
+import 'package:flutter/services.dart';
 import 'components/body_home.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarColor: Colors.white, statusBarBrightness: Brightness.light));
     return Scaffold(
       backgroundColor: Colors.grey[100],
       body: const BodyHome(),
